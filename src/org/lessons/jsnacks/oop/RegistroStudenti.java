17f7dbaebe6a8ce20beced9432423307e8bd0ddb
Dariya Kashtan <kashtandariya@gmail.com>
2024-05-16 19:33:16 +0200

    classe e costruttore registro studenti

--- src/org/lessons/jsnacks/oop/RegistroStudenti.java
+++ src/org/lessons/jsnacks/oop/RegistroStudenti.java
@@ -1,5 +1,20 @@
 package org.lessons.jsnacks.oop;
 
-public class RegistroStudenti {
+/*Utilizza un array privato per memorizzare oggetti Studente. V
+*Implementa un costruttore senza parametri per inizializzare il registro vuoto V
+*e un metodo pubblico per aggiungere studenti al registro.
+*Aggiungi un metodo che stampi la lista degli studenti.
+*/
 
+public class RegistroStudenti {
+	
+	private Studente [] studenti; //creazione array privato 
+	
+	public RegistroStudenti() {
+		this.studenti = new Studente[10]; //costruttore con indicazione n° max studenti nell'array
+	}
+	
+	
+	
+	
 }
