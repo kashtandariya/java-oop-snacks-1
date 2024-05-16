555dd2904b4787bac807062cad4885dee0131772
Dariya Kashtan <kashtandariya@gmail.com>
2024-05-16 19:28:37 +0200

    classe e costruttore studente come jsnacks 1

--- src/org/lessons/jsnacks/oop/Studente.java
+++ src/org/lessons/jsnacks/oop/Studente.java
@@ -1,5 +1,22 @@
 package org.lessons.jsnacks.oop;
 
+/*ripresa dall'esercizio fatto in classe con costruzione oggetto studente
+ * con relativi attributi private
+ * in seguito costruzione costruttore con gli attributi
+ * metodo per ritornare concatenazione di string e int
+ */
 public class Studente {
-
+	private String name;
+	private String surname;
+	private int age;
+	
+	public Studente(String name, String surname, int age) {
+		this.name = name;
+		this.surname = surname;
+		this.age = age;
+	}
+	
+	public String studenteStringified() {
+		return name + " " + surname + ", " + age + " anni";
+	}
 }
